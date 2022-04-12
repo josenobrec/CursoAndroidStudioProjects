eb80cf49c235907f82a7061c5bee8da6caa847c8
José Nobre <jnobrecf@gmail.com>
2022-04-12 17:47:23 -0300

    Aula 294

--- Instagram/app/src/main/java/com/cursoandroid/instagram/activity/FiltroActivity.java
+++ Instagram/app/src/main/java/com/cursoandroid/instagram/activity/FiltroActivity.java
@@ -13,11 +13,18 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.cursoandroid.instagram.R;
+import com.zomato.photofilters.FilterPack;
+import com.zomato.photofilters.imageprocessors.Filter;
 
 public class FiltroActivity extends AppCompatActivity {
 
+    {
+        System.loadLibrary("NativeImageProcessor");
+    }
+
     private ImageView imageFotoEscolhida;
     private Bitmap imagem;
+    private Bitmap imagemFiltro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,11 @@ public class FiltroActivity extends AppCompatActivity {
             byte[] dadosImagem = bundle.getByteArray("fotoEscolhida");
             imagem = BitmapFactory.decodeByteArray(dadosImagem, 0, dadosImagem.length);
             imageFotoEscolhida.setImageBitmap(imagem);
+
+            imagemFiltro = imagem.copy(imagem.getConfig(), true);
+           // Filter filter = FilterPack.getAdeleFilter(getApplicationContext());
+            Filter filter = FilterPack.getAmazonFilter(getApplicationContext());
+            imageFotoEscolhida.setImageBitmap(filter.processFilter(imagemFiltro));
         }
     }
 
