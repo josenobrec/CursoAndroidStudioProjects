896fd9457f4cac3c1022eca950ed94ee94ba1485
José Nobre <jnobrecf@gmail.com>
2022-04-12 16:00:42 -0300

    Aula 292

---
+++ Instagram/app/src/main/java/com/cursoandroid/instagram/activity/FiltroActivity.java
@@ -0,0 +1,34 @@
+package com.cursoandroid.instagram.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.cursoandroid.instagram.R;
+
+public class FiltroActivity extends AppCompatActivity {
+
+    private ImageView imageFotoEscolhida;
+    private Bitmap imagem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filtro);
+
+        //Inicializar componentes
+        imageFotoEscolhida = findViewById(R.id.imageFotoEscolhida);
+
+        //Recupera a imagem escolhida pelo usuário
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            byte[] dadosImagem = bundle.getByteArray("fotoEscolhida");
+            imagem = BitmapFactory.decodeByteArray(dadosImagem, 0, dadosImagem.length);
+            imageFotoEscolhida.setImageBitmap(imagem);
+        }
+
+    }
+}
--- Instagram/app/src/main/java/com/cursoandroid/instagram/fragment/PostagemFragment.java
+++ Instagram/app/src/main/java/com/cursoandroid/instagram/fragment/PostagemFragment.java
@@ -2,8 +2,11 @@ package com.cursoandroid.instagram.fragment;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.provider.MediaStore;
@@ -13,7 +16,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.cursoandroid.instagram.R;
+import com.cursoandroid.instagram.activity.FiltroActivity;
 import com.cursoandroid.instagram.helper.Permissao;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -107,5 +113,46 @@ public class PostagemFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode ==getActivity().RESULT_OK){
+
+            Bitmap imagem = null;
+
+            try {
+                //Valida tipo de seleçao da imagem
+                switch (requestCode){
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+
+                //Valida imagem selecionada
+                if(imagem != null){
+
+                    //Converter imagem em byte array
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+                    //Envia imagem escolhida para aplicação de filtro
+                    Intent i = new Intent(getActivity(), FiltroActivity.class);
+                    i.putExtra("fotoEscolhida", dadosImagem);
+                    startActivity(i);
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
     }
 }
