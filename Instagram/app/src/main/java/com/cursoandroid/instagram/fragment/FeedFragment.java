cd63171f3e757940c8269ecb23d9690d8364b16a
José Nobre <jnobrecf@gmail.com>
2022-04-20 14:14:52 -0300

    Aula 310

--- Instagram/app/src/main/java/com/cursoandroid/instagram/fragment/FeedFragment.java
+++ Instagram/app/src/main/java/com/cursoandroid/instagram/fragment/FeedFragment.java
@@ -2,13 +2,29 @@ package com.cursoandroid.instagram.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cursoandroid.instagram.R;
+import com.cursoandroid.instagram.adapter.AdapterFeed;
+import com.cursoandroid.instagram.helper.ConfiguracaoFirebase;
+import com.cursoandroid.instagram.helper.UsuarioFirebase;
+import com.cursoandroid.instagram.model.Feed;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +32,13 @@ import com.cursoandroid.instagram.R;
  * create an instance of this fragment.
  */
 public class FeedFragment extends Fragment {
+
+    private RecyclerView recyclerFeed;
+    private AdapterFeed adapterFeed;
+    private List<Feed> listaFeed = new ArrayList<>();
+    private ValueEventListener valueEventListenerFeed;
+    private DatabaseReference feedRef;
+    private String idUsuarioLogado;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +84,57 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View view =  inflater.inflate(R.layout.fragment_feed, container, false);
+
+        //Configurações iniciais
+        idUsuarioLogado = UsuarioFirebase.getIdentificadorUsuario();
+        feedRef = ConfiguracaoFirebase.getFirebase()
+                .child("feed")
+                .child(idUsuarioLogado);
+
+        //Inicializar componentes
+        recyclerFeed = view.findViewById(R.id.recyclerFeed);
+
+        //Configura recyclerview
+        adapterFeed = new AdapterFeed(listaFeed, getActivity());
+        recyclerFeed.setHasFixedSize(true);
+        recyclerFeed.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerFeed.setAdapter(adapterFeed);
+
+        return view;
+    }
+
+    private void listarFeed(){
+
+        valueEventListenerFeed = feedRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    listaFeed.add(ds.getValue(Feed.class));
+                }
+                Collections.reverse(listaFeed);
+                adapterFeed.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        listarFeed();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        feedRef.removeEventListener(valueEventListenerFeed);
     }
 }
